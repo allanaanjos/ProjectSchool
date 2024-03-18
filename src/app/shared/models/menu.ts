743bc9fc7c4285a0d11ab0d2a69d064569384758
allan silva <allananjosdasilva@gmail.com>
2024-03-18 15:01:34 -0300

    modulo criado

---
+++ src/app/shared/models/menu.ts
@@ -0,0 +1,29 @@
+import { MenuItem } from "./menuItem";
+
+export const menuItems: MenuItem[] = [
+  {
+    link: '/dashboard',
+    icon: 'fa-chart-line',
+    label: 'Dashboard',
+  },
+  {
+    link: '/courses',
+    icon: 'fa-graduation-cap',
+    label: 'Cursos',
+  },
+  {
+    link: '/teachers',
+    icon: 'fa-person-chelkboard',
+    label: 'Professores',
+  },
+  {
+    link: '/students',
+    icon: 'fa-chelkboard-user',
+    label: 'Estudantes',
+  },
+  {
+    link: '/support',
+    icon: 'fa-headset',
+    label: 'Suporte',
+  },
+]
